c518a5f35d4963e66cd67eae6c382179a2a229b8
yuyaebean <yuyaebean@gmail.com>
2020-08-14 22:25:18 +0900

    Create Counter component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { RecoilRoot, atom, useRecoilState } from 'recoil'
+
+const countState = atom({
+  key: 'countState',
+  default: 0,
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <RecoilRoot>
+      <Counter />
+    </RecoilRoot>
+  )
 }
 
-export default App;
+function Counter() {
+  const [count, setCount] = useRecoilState(countState)
+
+  function addCount() {
+    setCount(count + 1)
+  }
+
+  function subCount() {
+    setCount(count - 1)
+  }
+
+  return (
+    <div>
+      <button onClick={addCount}>up</button>
+      <button onClick={subCount}>down</button>
+      <p>{count}</p>
+    </div>
+  )
+}
+
+export default App
