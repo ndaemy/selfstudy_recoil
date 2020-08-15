a3bd4235755d7e0e55665b126911ea8f19be8e8e
yuyaebean <yuyaebean@gmail.com>
2020-08-16 01:55:26 +0900

    Add TodoList to Stats and Filters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import {
   atom,
+  selector,
   useRecoilState,
   useRecoilValue,
   useSetRecoilState,
@@ -12,18 +13,97 @@ const todoListState = atom({
   default: [],
 })
 
+const todoListFilterState = atom({
+  key: 'todoListFilterState',
+  default: 'Show All',
+})
+
+const filteredTodoListState = selector({
+  key: 'filteredTodoListState',
+  get: ({ get }) => {
+    const filter = get(todoListFilterState)
+    const list = get(todoListState)
+
+    switch (filter) {
+      case 'Show Completed':
+        return list.filter((item) => item.isComplete)
+      case 'Show Uncompleted':
+        return list.filter((item) => !item.isComplete)
+      default:
+        return list
+    }
+  },
+})
+
+const todoListStatsState = selector({
+  key: 'todoListStatsState',
+  get: ({ get }) => {
+    const todoList = get(todoListState)
+    const totalNum = todoList.length
+    const totalCompletedNum = todoList.filter((item) => item.isComplete).length
+    const totalUncompletedNum = totalNum - totalCompletedNum
+    const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum
+
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+      percentCompleted,
+    }
+  },
+})
+
 function TodoList() {
-  const todoList = useRecoilValue(todoListState)
+  const todoList = useRecoilValue(filteredTodoListState)
 
   return (
     <>
-      {/* <TodoListStats /> */}
-      {/* <TodoListFilters /> */}
+      <TodoListStats />
+      <TodoListFilters />
       <TodoItemCreator />
 
       {todoList.map((todoItem) => (
         <TodoItem key={todoItem.id} item={todoItem} />
       ))}
+    </>
+  )
+}
+
+function TodoListStats() {
+  const {
+    totalNum,
+    totalCompletedNum,
+    totalUncompletedNum,
+    percentCompleted,
+  } = useRecoilValue(todoListStatsState)
+
+  const formattedPercentCompleted = Math.round(percentCompleted * 100)
+
+  return (
+    <ul>
+      <li>Total items: {totalNum}</li>
+      <li>Items completed: {totalCompletedNum}</li>
+      <li>Items not completed: {totalUncompletedNum}</li>
+      <li>Percent completed: {formattedPercentCompleted}</li>
+    </ul>
+  )
+}
+
+function TodoListFilters() {
+  const [filter, setFilter] = useRecoilState(todoListFilterState)
+
+  const updateFilter = ({ target: { value } }) => {
+    setFilter(value)
+  }
+
+  return (
+    <>
+      Filter:
+      <select value={filter} onChange={updateFilter}>
+        <option value='Show All'>All</option>
+        <option value='Show Completed'>Completed</option>
+        <option value='Show Uncompleted'>Uncompleted</option>
+      </select>
     </>
   )
 }
